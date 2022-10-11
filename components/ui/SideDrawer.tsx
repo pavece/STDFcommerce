2c86f9572536ea7eb140080ff049677a30c305fe
Pavece <55211797+pavece@users.noreply.github.com>
2022-10-11 16:45:03 +0200

    Style details and data for the side menu

--- components/ui/SideDrawer.tsx
+++ components/ui/SideDrawer.tsx
@@ -1,7 +1,23 @@
 import { useContext } from "react";
-import { Drawer, ListItem, List, Typography, Divider } from "@mui/material";
+import {
+  Drawer,
+  ListItem,
+  List,
+  Typography,
+  Divider,
+  Box,
+  ListItemIcon,
+  ListItemText,
+} from "@mui/material";
 import { UiContext } from "../../context/uiContext/uiContext";
-
+import PersonAddAlt1RoundedIcon from "@mui/icons-material/PersonAddAlt1Rounded";
+import KeyRoundedIcon from "@mui/icons-material/KeyRounded";
+import QueryStatsRoundedIcon from "@mui/icons-material/QueryStatsRounded";
+import Inventory2RoundedIcon from "@mui/icons-material/Inventory2Rounded";
+import PeopleAltRoundedIcon from "@mui/icons-material/PeopleAltRounded";
+import LocalShippingRoundedIcon from "@mui/icons-material/LocalShippingRounded";
+import HouseRoundedIcon from "@mui/icons-material/HouseRounded";
+import AssignmentTurnedInRoundedIcon from "@mui/icons-material/AssignmentTurnedInRounded";
 export const SideDrawer = () => {
   const interfaceContext = useContext(UiContext);
 
@@ -11,18 +27,107 @@ export const SideDrawer = () => {
       open={interfaceContext.sideMenuOpen}
       onClose={() => interfaceContext.uiCloseSideMenu()}
       sx={{
-        minWidth: "300px",
+        backdropFilter: "blur(6px)",
+        transition: "all 0.5s ease-out",
       }}
     >
-      <List>
-        <ListItem>
-          <Typography>Sign in</Typography>
+      <Box
+        sx={{
+          width: 250,
+          paddingTop: 5,
+        }}
+      >
+        <List>
+          {/* Search bar implementation for mobile users */}
+
+          <ListItem
+            sx={{
+              cursor: "pointer",
+            }}
+          >
+            {/* User is not logged in */}
+            <ListItemIcon>
+              <KeyRoundedIcon />
+            </ListItemIcon>
+            <ListItemText primary="Log In" />
+          </ListItem>
+          <ListItem
+            sx={{
+              cursor: "pointer",
+            }}
+          >
+            <ListItemIcon>
+              <PersonAddAlt1RoundedIcon />
+            </ListItemIcon>
+            <ListItemText primary="Register" />
+          </ListItem>
+
+          {/* User is logged in */}
+          <ListItem
+            sx={{
+              cursor: "pointer",
+            }}
+          >
+            <ListItemIcon>
+              <HouseRoundedIcon />
+            </ListItemIcon>
+            <ListItemText primary="My address" />
+          </ListItem>
+
+          <ListItem
+            sx={{
+              cursor: "pointer",
+            }}
+          >
+            <ListItemIcon>
+              <AssignmentTurnedInRoundedIcon />
+            </ListItemIcon>
+            <ListItemText primary="My Orders" />
+          </ListItem>
+
+          {/* User is admin */}
+          <ListItem
+            sx={{
+              cursor: "pointer",
+            }}
+          >
+            <ListItemIcon>
+              <QueryStatsRoundedIcon />
+            </ListItemIcon>
+            <ListItemText primary="Stats" />
+          </ListItem>
+          <ListItem
+            sx={{
+              cursor: "pointer",
+            }}
+          >
+            <ListItemIcon>
+              <Inventory2RoundedIcon />
+            </ListItemIcon>
+            <ListItemText primary="Products" />
+          </ListItem>
+          <ListItem
+            sx={{
+              cursor: "pointer",
+            }}
+          >
+            <ListItemIcon>
+              <PeopleAltRoundedIcon />
+            </ListItemIcon>
+            <ListItemText primary="Clients" />
+          </ListItem>
+        </List>
+        <ListItem
+          sx={{
+            cursor: "pointer",
+          }}
+        >
+          <ListItemIcon>
+            <LocalShippingRoundedIcon />
+          </ListItemIcon>
+          <ListItemText primary="Orders" />
         </ListItem>
-        <Divider />
-        <ListItem>
-          <Typography>Admin</Typography>
-        </ListItem>
-      </List>
+      </Box>
     </Drawer>
   );
 };
